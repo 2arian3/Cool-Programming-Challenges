dfc648f66bcfd66453a7d3abb9d8c36ece8bf8e9
Macbook Pro <aryan.boukani2@gmail.com>
2020-08-01 02:59:52 +0430

    cleaned the code. it's almost done.

--- Tic-Tac-Toe/graphics.py
+++ Tic-Tac-Toe/graphics.py
@@ -4,38 +4,68 @@ import pygame
 import pygame_widgets
 
 '''colors'''
-BLACK = (0, 0, 0)
+#        R    G    B
+BLACK = (0  , 0  , 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+RED   = (255, 0  , 0)
 
 '''constants'''
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH  = 1000
 SCREEN_HEIGHT = 1000
-BLOCK_SIZE = 250
+BLOCK_SIZE    = 100
+START_X = (SCREEN_WIDTH - 3 * BLOCK_SIZE) // 2
+START_Y = (SCREEN_HEIGHT - 3 * BLOCK_SIZE) // 2
+BOARD_CORNERS = [(START_X, START_Y), (START_X + 3*BLOCK_SIZE, START_Y + 3*BLOCK_SIZE)]
 
-'''returns true if the given position is in the board'''
-def inTheBoard(position, corners):
-    top_left, bottom_right = corners
+'''returns true if the given position is in the board.'''
+def inTheBoard(position):
+    top_left, bottom_right = BOARD_CORNERS
     return top_left[0] <= position[0] <= bottom_right[0] and top_left[1] <= position[1] <= bottom_right[1]
 
-startX = (SCREEN_WIDTH - 3 * BLOCK_SIZE) // 2
-startY = (SCREEN_HEIGHT - 3 * BLOCK_SIZE) // 2
-boardCorners = [(startX, startY), (startX + 3*BLOCK_SIZE, startY + 3*BLOCK_SIZE)]
+'''drawing the board based on the constants.'''
+def drawBoard(screen, startingPosition):
+    startX, startY = startingPosition
+    pygame.draw.line(screen, BLACK, (START_X, START_Y + BLOCK_SIZE), (START_X + 3*BLOCK_SIZE, START_Y + BLOCK_SIZE), 2)
+    pygame.draw.line(screen, BLACK, (START_X, START_Y + 2*BLOCK_SIZE), (START_X + 3*BLOCK_SIZE, START_Y + 2*BLOCK_SIZE), 2)
+    pygame.draw.line(screen, BLACK, (START_X + BLOCK_SIZE, START_Y), (START_X + BLOCK_SIZE, START_Y + 3*BLOCK_SIZE), 2)
+    pygame.draw.line(screen, BLACK, (START_X + 2*BLOCK_SIZE, START_Y), (START_X + 2*BLOCK_SIZE, START_Y + 3*BLOCK_SIZE), 2)
+
+'''creating the players icons.'''
+def creatingTheIcons(xColor, oColor):
+    x, o = pygame.Surface((BLOCK_SIZE-5, BLOCK_SIZE-5)), pygame.Surface((BLOCK_SIZE-5, BLOCK_SIZE-5))
+    x.fill(WHITE)
+    o.fill(WHITE)
+    pygame.draw.circle(o, oColor, (BLOCK_SIZE // 2, BLOCK_SIZE // 2), BLOCK_SIZE // 2 - 5, 2)
+    pygame.draw.aaline(x, xColor, (5, 5), (BLOCK_SIZE - 5, BLOCK_SIZE - 5), 1)
+    pygame.draw.aaline(x, xColor, (5, BLOCK_SIZE - 5), (BLOCK_SIZE - 5, 5), 1)
+    return x, o
+
+def playerMove(screen, position):
+    legal = False
+    posX, posY = position
+    posX = ((posX - START_X) // BLOCK_SIZE) * BLOCK_SIZE + START_X
+    posY = ((posY - START_Y) // BLOCK_SIZE) * BLOCK_SIZE + START_Y
+    j, i = (posX - START_X) // BLOCK_SIZE, (posY - START_Y) // BLOCK_SIZE
+    if logic.placeIsFree(logic.board, 3*i + j):
+        logic.board[3*i + j] = logic.user
+        screen.blit(x, (posX + 2, posY + 2))
+        legal = True
+    pygame.display.update()
+    return legal
+
+def cpuMove(screen):
+    move = logic.cpuMove(logic.board, logic.cpu, logic.user)
+    posY = move // 3
+    posX = move % 3
+    screen.blit(o, (START_X + posX*BLOCK_SIZE + 2, START_Y + posY*BLOCK_SIZE + 2))
+    pygame.display.update()
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
-pygame.display.set_caption('Tic-Tac-Toe')
-x, o = pygame.Surface((BLOCK_SIZE-5, BLOCK_SIZE-5)), pygame.Surface((BLOCK_SIZE-5, BLOCK_SIZE-5))
-x.fill(WHITE)
-o.fill(WHITE)
 screen.fill(WHITE)
-pygame.draw.circle(o, RED, (BLOCK_SIZE // 2, BLOCK_SIZE // 2), BLOCK_SIZE // 2 - 5, 2)
-pygame.draw.aaline(x, BLACK, (5, 5), (BLOCK_SIZE - 5, BLOCK_SIZE - 5), 1)
-pygame.draw.aaline(x, BLACK, (5, BLOCK_SIZE - 5), (BLOCK_SIZE - 5, 5), 1)
-pygame.draw.line(screen, BLACK, (startX, startY + BLOCK_SIZE), (startX + 3*BLOCK_SIZE, startY + BLOCK_SIZE), 2)
-pygame.draw.line(screen, BLACK, (startX, startY + 2*BLOCK_SIZE), (startX + 3*BLOCK_SIZE, startY + 2*BLOCK_SIZE), 2)
-pygame.draw.line(screen, BLACK, (startX + BLOCK_SIZE, startY), (startX + BLOCK_SIZE, startY + 3*BLOCK_SIZE), 2)
-pygame.draw.line(screen, BLACK, (startX + 2*BLOCK_SIZE, startY), (startX + 2*BLOCK_SIZE, startY + 3*BLOCK_SIZE), 2)
+pygame.display.set_caption('Tic-Tac-Toe')
+drawBoard(screen, (START_X, START_Y))
+x, o = creatingTheIcons(BLACK, RED)
 pygame.display.update()
 
 '''game loop'''
@@ -46,18 +76,8 @@ while running:
         running = False
     if event.type == pygame.MOUSEBUTTONDOWN:
         posX, posY = event.pos
-        if inTheBoard((posX, posY), boardCorners):
-            posX = ((posX - startX) // BLOCK_SIZE) * BLOCK_SIZE + startX
-            posY = ((posY - startY) // BLOCK_SIZE) * BLOCK_SIZE + startY
-            j, i = (posX - startX) // BLOCK_SIZE, (posY - startY) // BLOCK_SIZE
-            if logic.placeIsFree(logic.board, 3*i + j):
-                logic.board[3*i + j] = logic.user
-                screen.blit(x, (posX + 2, posY + 2))
-            pygame.display.update() 
-            time.sleep(1)
-            cpuMove = logic.cpuMove(logic.board, logic.cpu, logic.user)
-            posY = cpuMove // 3
-            posX = cpuMove % 3
-            screen.blit(o, (startX + posX*BLOCK_SIZE + 2, startY + posY*BLOCK_SIZE + 2))
+        if inTheBoard((posX, posY)):
+            if playerMove(screen, (posX, posY)):
+                time.sleep(1)
+                cpuMove(screen)
     pygame.display.update()
-    
