4221502f03af025d314cf128e3564f036e326f16
Macbook Pro <aryan.boukani2@gmail.com>
2020-09-10 21:10:32 +0430

    now we can move the shapes on the board

--- Tetris/main.py
+++ Tetris/main.py
@@ -4,6 +4,7 @@ from shapes import shapes, colors
 
 pygame.init()
 
+#Constants
 WIDTH        = 800
 HEIGHT       = 800
 BLOCK_SIZE   = 35
@@ -21,9 +22,9 @@ first_menu_text = FONT1.render('To start the game, press any button', True, (255
 producer = FONT3.render('produced by arian boukani', True, (255, 0, 0))
 version = FONT3.render('version 1.0', True, (255, 0, 0))
 
+score = 0
 start_x = (WIDTH - BOARD_WIDTH) // 2
 start_y = (HEIGHT - BOARD_HEIGHT) // 2
-cell_colors = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
 
 class Shape:
 
@@ -32,7 +33,17 @@ class Shape:
         self.y = row
         self.shape = shape
         self.color = colors[shapes.index(shape)]
-        self.rotation = 0
+        self.rotations = 0
+
+def shape_handling(shape):
+    block_positions = []
+    s = shape.shape[shape.rotations % len(shape.shape)]
+    for i, line in enumerate(s):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '*':
+                block_positions.append((shape.x + j - 2, shape.y + i - 4))
+    return block_positions
 
 def draw_first_menu():
     window.blit(tetris, (start_x + 5*BLOCK_SIZE - tetris.get_width() // 2, 10))
@@ -41,7 +52,7 @@ def draw_first_menu():
     window.blit(version, (WIDTH // 2 - version.get_width() // 2, HEIGHT -  version.get_height()))
     pygame.display.update()
     
-def draw_game_menu():
+def draw_game_menu(cell_colors):
     window.fill((0, 0, 0))
     window.blit(tetris, (start_x + 5*BLOCK_SIZE - tetris.get_width() // 2, 10))
     for i in range(20):
@@ -52,7 +63,7 @@ def draw_game_menu():
         pygame.draw.line(window, (128, 128, 128), (start_x, start_y + i*BLOCK_SIZE), (start_x + 10*BLOCK_SIZE, start_y + i*BLOCK_SIZE))
     for i in range(1, 10):
         pygame.draw.line(window, (128, 128, 128), (start_x + i*BLOCK_SIZE, start_y), (start_x + i*BLOCK_SIZE, start_y + 20*BLOCK_SIZE))
-    draw_next_shape(get_shape())
+    # draw_next_shape(get_shape())
     pygame.display.update()
 
 def get_shape():
@@ -68,22 +79,49 @@ def draw_next_shape(shape):
             if column is '*':
                 pygame.draw.rect(window, shape.color, (x + j*BLOCK_SIZE, y + i*BLOCK_SIZE, 34, 34), 0)
 
+def generate_cell_colors(locked_positions):
+    cell_colors = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
+    for i in range(20):
+        for j in range(10):
+            if (j, i) in locked_positions:
+                cell_colors[i][j] = locked_positions[(j, i)]
+    return cell_colors
+
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 window.fill((0, 0, 0))
 pygame.display.set_caption('TETRIS')
 
-def main():   
-    draw_game_menu()
+def main(): 
+    locked_positions = dict() 
+    cell_colors = generate_cell_colors(locked_positions) 
     running = True
-
     current_shape = get_shape()
     next_shape = get_shape()
+    draw_game_menu(cell_colors)
 
     while running:
+        cell_colors = generate_cell_colors(locked_positions)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_shape.x -= 1
+                
+                elif event.key == pygame.K_RIGHT:
+                    current_shape.x += 1
+                
+                elif event.key == pygame.K_DOWN:
+                    current_shape.y += 1
 
+                elif event.key == pygame.K_UP:
+                    current_shape.rotation += 1
+                    current_shape.rotation %= len(current_shape.shape)
+        shape_positions = shape_handling(current_shape)
+        for x, y in shape_positions:
+            if y >= 0:
+                cell_colors[y][x] = current_shape.color
+        draw_game_menu(cell_colors)
 def first_menu():
     draw_first_menu()
     running = True
@@ -97,3 +135,6 @@ def first_menu():
 
 if __name__ == '__main__':
     first_menu()
+
+pygame.display.quit()
+quit()
